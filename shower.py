88be8fc4651eccc9b298d3648e0d7055b03f0277
David Jones <dave01945@gmail.com>
2023-04-01 18:45:08 +0100

    client id

--- shower.py
+++ shower.py
@@ -4,8 +4,8 @@ import sys
 # This is the BLE address of the device
 address = "ce:d7:b8:34:aa:e4"
 # See below for how to obtain the device_id and client_id
-device_id = 4
-client_id = 63519
+device_id = 1
+client_id = 24829
 
 if len(sys.argv) > 1:
     if sys.argv[1] == '-h':
