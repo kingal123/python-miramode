e5d31d4a36aad8593ebe77786ffe8e84380d3fe1
kingal123 <70146605+kingal123@users.noreply.github.com>
2023-11-21 21:14:14 +0000

    Update showerOn.py
    Updated Client_ID

--- showerOn.py
+++ showerOn.py
@@ -1,10 +1,10 @@
 import miramode
 
 # This is the BLE address of the device
-address = "ce:d7:b8:34:aa:e4"
+address = "c1:73:51:1a:08:ec"
 # See below for how to obtain the device_id and client_id
-device_id = 4
-client_id = 63519
+device_id = 1
+client_id = 5559
 
 # Turn on the 1st outlet with a 40C degrees water temperature
 miramode.control_outlets(address, device_id, client_id, True, False, 39)
